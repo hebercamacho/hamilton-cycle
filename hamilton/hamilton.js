7e94c33fe6ff6eb1c6d0036fffbe6349f86bc374
Héber Camacho <heber.camachod@gmail.com>
2022-08-07 01:27:52 -0300

    adicionando esboço da função de busca

--- hamilton/hamilton.js
+++ hamilton/hamilton.js
@@ -2,10 +2,22 @@
 var No = /** @class */ (function () {
     function No(leader, id, weight) {
         if (weight === void 0) { weight = 1; }
+        this.getVizinhosDisponiveis = function () {
+            var cont = 0;
+            //iteracao por todos os vizinhos
+            var aux = this.leader;
+            while (aux != undefined) {
+                if (aux.passouPorAqui)
+                    cont++;
+                aux = this.leader;
+            }
+            return cont;
+        };
         //retorna true se é válido colocar este nó na posição k
         this.isValid = function (k) {
             if (this.passouPorAqui)
                 return false;
+            //iteracao por todos os vizinhos
             var aux = this.leader;
             while (aux != undefined) {
                 if (this.id == k - 1)
@@ -14,6 +26,7 @@ var No = /** @class */ (function () {
             }
             return false;
         };
+        // this.grau++;
         this.id = id;
         this.weight = weight;
         this.leader = leader;
@@ -28,10 +41,26 @@ var Grafo = /** @class */ (function () {
             if (weight === void 0) { weight = 1; }
             this.adj[node1] = new No(this.adj[node1], node2, weight);
             this.adj[node2] = new No(this.adj[node2], node1, weight);
-            this.edges++;
+            this.arestas++;
+        };
+        this.possuiCicloHamiltoniano = function (noInicial) {
+            //eliminar grafos inválidos
+            if (this.adj.find(function (n) { return !n || n.getVizinhosDisponiveis() < 2; }))
+                return false;
+            //inicializações necessarias
+            this.adj[noInicial].passouPorAqui = true;
+            var proximoVizinho = this.adj[noInicial].id;
+            var caminho = [noInicial];
+            //iteração
+            for (; caminho.length <= this.vertices; caminho.push(proximoVizinho)) {
+                while (this.adj[proximoVizinho] != undefined && this.adj[proximoVizinho].passouPorAqui) {
+                    proximoVizinho = this.adj[proximoVizinho].leader;
+                }
+            }
+            return caminho.length == this.vertices;
         };
         this.vertices = vertices; //número de vertices
-        this.edges = 0; //número de arestas
+        this.arestas = 0; //número de arestas
         for (var index = 0; index < vertices; index++) {
             this.adj = new Array(vertices);
         }
@@ -48,3 +77,5 @@ G.inserirAresta(1, 3);
 G.inserirAresta(1, 4);
 G.inserirAresta(2, 4);
 G.inserirAresta(3, 4);
+G.adj.map(function (e) { return console.log(e); });
+G.possuiCicloHamiltoniano(0);
--- hamilton/hamilton.ts
+++ hamilton/hamilton.ts
@@ -3,21 +3,34 @@ class No {
     id: number;//valor ou id do vértice
     weight: number; //peso da aresta entre o vértice e seu leader
     leader: No; //ponteiro o para vértice de cima na árvore
-    passouPorAqui: boolean;
+    passouPorAqui: boolean; //flag indicando se o algoritmo já passou por este nó
+    vizinhosDisponiveis: number; //indica quantos vizinhos ainda possuem passouPorAqui=true
     constructor(leader: No, id: number, weight = 1) {
+        // this.grau++;
         this.id = id;
         this.weight = weight;
         this.leader = leader;
         this.passouPorAqui = false;
     }
 
-    //retorna true se é válido colocar este nó na posição k
-    isValid = function(k: number): boolean{
-        if(this.passouPorAqui) return false;
-
+    getVizinhosDisponiveis = function (): number {
+        let cont = 0;
+        //iteracao por todos os vizinhos
         let aux = this.leader;
-        while(aux != undefined){
-            if(this.id == k-1) return true;
+        while (aux != undefined) {
+            if (aux.passouPorAqui) cont++;
+            aux = this.leader;
+        }
+        return cont;
+    }
+
+    //retorna true se é válido colocar este nó na posição k
+    isValid = function (k: number): boolean {
+        if (this.passouPorAqui) return false;
+        //iteracao por todos os vizinhos
+        let aux = this.leader;
+        while (aux != undefined) {
+            if (this.id == k - 1) return true;
             aux = this.leader;
         }
         return false;
@@ -26,11 +39,11 @@ class No {
 
 class Grafo {
     vertices: number;
-    edges: number;
+    arestas: number;
     adj: No[];
     constructor(vertices: number) {
         this.vertices = vertices; //número de vertices
-        this.edges = 0; //número de arestas
+        this.arestas = 0; //número de arestas
         for (let index = 0; index < vertices; index++) {
             this.adj = new Array<No>(vertices);
         }
@@ -39,18 +52,37 @@ class Grafo {
     inserirAresta = function (node1: number, node2: number, weight = 1) {
         this.adj[node1] = new No(this.adj[node1], node2, weight);
         this.adj[node2] = new No(this.adj[node2], node1, weight);
-        this.edges++;
+        this.arestas++;
+    }
+
+    possuiCicloHamiltoniano = function(noInicial: number): boolean {
+        //eliminar grafos inválidos
+        if(this.adj.find(n => !n || n.getVizinhosDisponiveis() < 2)) return false;
+
+        //inicializações necessarias
+        this.adj[noInicial].passouPorAqui = true;
+        let proximoVizinho = this.adj[noInicial].id;
+        let caminho = [noInicial]
+        //iteração
+        for (; caminho.length <= this.vertices; caminho.push(proximoVizinho)) {
+            while(this.adj[proximoVizinho] != undefined && this.adj[proximoVizinho].passouPorAqui){
+                proximoVizinho = this.adj[proximoVizinho].leader;
+            }
+        }
+        return caminho.length == this.vertices;
     }
 };
 
 //teste mockado
 let G: Grafo = new Grafo(5);
 
-G.inserirAresta(0,1);
-G.inserirAresta(0,3);
-G.inserirAresta(1,2);
-G.inserirAresta(1,3);
-G.inserirAresta(1,4);
-G.inserirAresta(2,4);
-G.inserirAresta(3,4);
+G.inserirAresta(0, 1);
+G.inserirAresta(0, 3);
+G.inserirAresta(1, 2);
+G.inserirAresta(1, 3);
+G.inserirAresta(1, 4);
+G.inserirAresta(2, 4);
+G.inserirAresta(3, 4);
 
+G.adj.map(e => console.log(e));
+G.possuiCicloHamiltoniano(0);
